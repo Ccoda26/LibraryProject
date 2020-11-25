36f811bbbc854ee4c7780c61fbbfd2f892c1fd0b
Csoda26 <charlie.clavel@lapiscine.pro>
2020-11-25 17:20:31 +0100

    delete article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,9 @@
 namespace App\Controller;
 
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -36,5 +38,71 @@ class ArticleController extends AbstractController
                 'article' => $article
             ]);
         }
+    }
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     * @Route("/article/insert", name="page_insert")
+     */
+
+    public function insertArticle(EntityManagerInterface $entityManager){
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $article = new Article();
+        $article->setTitle('La plus valus du congo');
+        $article->setContent("La congolexicomatisation c'est inndispensable au congo");
+        $article->setPicture('https://images-na.ssl-images-amazon.com/images/I/71IHBrW5CiL._AC_SY450_.jpg');
+        $article->setCreationdate(new \DateTime());
+        $article->setPublicationdate(new \DateTime());
+        $article->setPublished(1);
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($article);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return $this->render('insertArticle.html.twig');
+
+    }
+
+    /**
+     * @param ArticleRepository $articleRepository
+     * @param EntityManagerInterface $entityManager
+     * @Route("/update-article/{id}", name="update_article")
+     */
+
+    public function updateArticle(ArticleRepository $articleRepository, EntityManagerInterface $entityManager, $id){
+
+        $article = $articleRepository->find($id);
+
+        $article->setTitle("Le perroquet");
+        $article->setContent("Il est beau, il est bon mon perroquet, yen auras pas pour tout le monde");
+
+        $entityManager->persist($article);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return $this->render('updatesArticle.html.twig');
+    }
+    /**
+     * @param ArticleRepository $articleRepository
+     * @param EntityManagerInterface $entityManager
+     * @Route("/delete-article/{id}", name="delete_article")
+     */
+    public function deleteArticle(ArticleRepository $articleRepository, $id, EntityManagerInterface $entityManager){
+
+
+        // select l'id pour updates la bonne ligne
+        $article = $articleRepository->find($id);
+
+
+        if (!is_null($article)){
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->render('deleteArticle.html.twig');
     }
 }
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,7 +3,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,7 +16,7 @@ class CategoryController extends AbstractController
      * @Route("/category/list", name="category_List")
      */
 
-    public function CatégoryList(CategoryRepository $categoryRepository){
+    public function CategoryList(CategoryRepository $categoryRepository){
         /* findAll() = select * from catégory */
         $categories = $categoryRepository ->findAll();
 
@@ -35,4 +37,52 @@ class CategoryController extends AbstractController
             'category' => $categories
         ]);
     }
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     * @Route("/category/insert", name="category_insert")
+     */
+
+    // entityMangaer et la methodes pour Insert en sql
+    public function insertCategory(EntityManagerInterface $entityManager){
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $category = new Category();
+        $category->setTitle('Pink Power');
+        $category->setColor("pink");
+        $category->setCreationdate(new \DateTime());
+        $category->setPublicationdate(new \DateTime());
+        $category->setPublished(0);
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($category);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return $this->render('categoryInsert.html.twig');
+
+    }
+
+    /**
+     * @Route("/update-category/{id}", name="update_category")
+     */
+
+    public function updatecategory(categoryRepository $categoryRepository, EntityManagerInterface $entityManager, $id){
+        // select l'id pour updates la bonne ligne
+        $category = $categoryRepository->find($id);
+
+        $category->setTitle("Le soleil");
+        $category->setColor("yellow");
+
+        // pré-enregistre les informations avant l'envoi
+        $entityManager->persist($category);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+        // envoi les infos vers mon fichier twig pour les afficher
+        return $this->render('updatesCategory.html.twig');
+    }
+
+
 }
